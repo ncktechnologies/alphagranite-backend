862a13e68f112dd4e322b1d7f08c5e11e88e47c3
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 <segzyboiy@gmail.com>
2025-11-19 16:13:09 +0100

    fix: Make account_id nullable in business_jobs table
    - Created migration to alter account_id column to allow NULL values
    - Fixes IntegrityError when creating jobs without account assignment
    - Migration: 20251119_081035_make_account_id_nullable.py

---
+++ alembic/versions/20251119_081035_make_account_id_nullable.py
@@ -0,0 +1,36 @@
+"""make_account_id_nullable
+
+Revision ID: 20251119_081035
+Revises: 20251119_073538
+Create Date: 2025-11-19 16:10:35
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '20251119_081035'
+down_revision: Union[str, Sequence[str], None] = '20251119_073538'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Make account_id column nullable in business_jobs table."""
+    
+    # Make account_id nullable
+    op.alter_column('business_jobs', 'account_id',
+                    existing_type=sa.Integer(),
+                    nullable=True)
+
+
+def downgrade() -> None:
+    """Revert account_id to NOT NULL."""
+    
+    # Make account_id NOT NULL again
+    op.alter_column('business_jobs', 'account_id',
+                    existing_type=sa.Integer(),
+                    nullable=False)
