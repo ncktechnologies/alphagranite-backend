fc00fcfbf365f5dd9a5c3daaba6f584c4adc8596
Chuks Ugwuh <chuks@ncktech.com>
2026-01-13 16:32:52 +0100

    Fix foreign key reference for cost_of_stone_id in FAB model

--- src/app/database/fab.py
+++ src/app/database/fab.py
@@ -72,4 +72,4 @@ class Fab(SQLModel, table=True):
     created_by: int = Field(foreign_key="users.id")
     updated_at: Optional[datetime] = None
     updated_by: Optional[int] = Field(default=None, foreign_key="users.id")
-    cost_of_stone_id: Optional[int] = Field(default=None, foreign_key="cost_of_stone.id")
+    cost_of_stone_id: Optional[int] = Field(default=None, foreign_key="cost_of_stones.id")
